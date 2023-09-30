7b74c03713d3abf4689590e927aae1f4de434a3e
Guillaume Caretto <guillaumecaretto@MacBook-Pro-de-Guillaume.local>
2023-09-30 12:59:06 +0200

    Adding Main + Builder methodes

--- Project02Eclipse/src/com/hemebiotech/analytics/Builder.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Builder.java
@@ -4,21 +4,20 @@ import com.hemebiotech.analytics.counter.SymptomCounter;
 import com.hemebiotech.analytics.reader.SymptomReader;
 import com.hemebiotech.analytics.writer.SymptomWriter;
 
-public class Analytics {
+public class Builder {
 
     private final SymptomReader reader;
     private final SymptomCounter counter;
     private final SymptomWriter writer;
 
-    public Analytics(String from, String to) {
+    public Builder(String from, String to) {
         this.reader = new SymptomReader(from);
         this.counter = new SymptomCounter();
         this.writer = new SymptomWriter(to);
     }
 
 
-    public void generateSymptomsResult() {
-        this.reader.getSymptoms();
+    public void buildSymptomsResult() {
         this.counter.setSymptoms(this.reader.getSymptoms());
         this.counter.count();
         this.writer.write(this.counter.getSortedSymptoms());
--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,39 +1,11 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+public class Main {
 
-public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String[] args) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	private static Builder builder;
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		reader.close();
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dilated pupils: " + pupilCount + "\n");
-		writer.close();
+	public static void main(String[] args) {
+		builder = new Builder("symptoms.txt", "result.out");
+		builder.buildSymptomsResult();
 	}
 }
